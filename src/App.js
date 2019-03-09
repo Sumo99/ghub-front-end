d1ab10a122694794b24e583d919a54ffb1c86875
Brandon G-Home <brandon.gardner2@outlook.com>
2019-03-09 12:08:04 -0600

    Route boilerplate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import "./App.css";
+
+import { Provider } from './context';
 
 class App extends Component {
   render() {
-    return <div className="App">Hello World</div>;
+    return (
+      <Provider>
+        <Router>
+          <div className="App">
+          {/*Header / Navigation here*/}
+            <div className="container">
+              <Switch>
+                <Route exact path='/' component={} />
+                <Route path='/results/:user' component={} />
+                <Route component={} /> {/*Not Found Component here*/}
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </Provider>
+      )
   }
 }
 
