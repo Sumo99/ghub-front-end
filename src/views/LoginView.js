c451390eb6f065c17a1f9c80ce466d58a2a24b51
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-03-11 15:50:19 -0500

    :sparkles: Add formhandling methods

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 
 export default class LoginView extends Component {
+  state = {
+    email: '',
+    password: '',
+  }
+
+  handleInput = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      email: '',
+      password: '',
+    })
+    this.props.history.push('/');
+  }
+
   render() {
     return (
         <div className="d-flex flex-column flex-row flex-items-center flex-justify-center py-4 my-4">
           <h2 className="text-shadow-light f2-light d-block py-3">
             Sign in to GitHub User Breakdown
           </h2>
-          <div className="Box" style={{ width: "20%" }}>
+          <div className="Box box-shadow" style={{ width: "20%" }}>
             <div className="Box-body">
-              <form>
+              <form onSubmit={this.handleSubmit}>
                 <fieldset className="my-2">
                   <label className="d-block mb-2" htmlFor="email">
                     Email Address
@@ -17,9 +35,12 @@ export default class LoginView extends Component {
                   <input
                     className="form-control width-full mb-2"
                     id="email"
+                    name="email"
                     type="email"
                     placeholder="Email address"
                     aria-label="Email address"
+                    onChange={this.handleInput}
+                    value={this.state.email}
                   />
                 </fieldset>
                 <fieldset className="mb-2">
@@ -29,9 +50,12 @@ export default class LoginView extends Component {
                   <input
                     className="form-control width-full mb-2"
                     id="password"
+                    name="password"
                     type="password"
                     placeholder="Password"
                     aria-label="Password"
+                    onChange={this.handleInput}
+                    value={this.state.password}
                   />
                 </fieldset>
               </form>
@@ -43,6 +67,7 @@ export default class LoginView extends Component {
               <button 
                 className="btn btn-small btn-primary mx-1" 
                 type="submit"
+                onClick={this.handleSubmit}
               >
                 Sign In
               </button>
