e1ef8bf8aaed848995d852b040eb0b4bf706cec7
Brandon G-Home <brandon.gardner2@outlook.com>
2019-03-11 13:42:52 -0500

    Header created with placeholder functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.scss";
 
-import { Provider } from './context';
+import { Provider } from "./context";
+import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <Provider>
       <Router>
         <div className="App">
-        {/*Header / Navigation here*/}
+          <Header />
           <div className="container">
             <Switch>
-              <Route exact path='/dashboard' component={} />
+              {/*<Route exact path='/dashboard' component={} />
               <Route path='/login' component={} />
               <Route path='/results/:user' component={} />
-              <Route component={} /> {/*Not Found Component here*/}
-              <Redirect from='/' to='/login' />
+              <Route component={} />
+              <Redirect from='/' to='/login' />*/}
             </Switch>
           </div>
         </div>
       </Router>
     </Provider>
-  )
-}
+  );
+};
 
 export default App;
