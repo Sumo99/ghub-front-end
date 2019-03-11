fe8d122d34fa5f751a817097ef2cac28146f6ef4
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-03-11 13:21:47 -0500

    :fire: Removing "container" class from App container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import "./App.scss";
 
 import { Provider } from './context';
+import LoginView from './views/LoginView';
 
 const App = () => {
   return (
@@ -10,12 +11,12 @@ const App = () => {
       <Router>
         <div className="App">
         {/*Header / Navigation here*/}
-          <div className="container">
+          <div>
             <Switch>
-              <Route exact path='/dashboard' component={} />
-              <Route path='/login' component={} />
-              <Route path='/results/:user' component={} />
-              <Route component={} /> {/*Not Found Component here*/}
+              {/* <Route exact path='/dashboard' component={} /> */}
+              <Route path='/login' component={LoginView} />
+              {/* <Route path='/results/:user' component={} /> */}
+              {/* <Route component={} /> Not Found Component here */}
               <Redirect from='/' to='/login' />
             </Switch>
           </div>
