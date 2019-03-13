f993b498517ea5651e176b121ba92fcd9c3a2dbd
Peter Murphy <peter.francis.murphy@gmail.com>
2019-03-13 13:05:31 -0400

    :sparkles: Adds dodge (D3 utility function)

---
+++ src/lib/dodge.js
@@ -0,0 +1,58 @@
+// These are pseudo types to help me remember what to pass in here :)
+
+// type Data = forall r. { value :: Number | r }
+// type Circle = { x :: Number, y :: Number, data :: Data }
+
+// dodge
+//  :: ( data    :: Data
+//     , radius  :: Number
+//     , x       :: Number -> Number
+//     , epsilon :: Maybe Number
+//     )
+//  -> [Circle]
+export default (data, radius, x, epsilon = 1e-4) => {
+  const radius2 = radius ** 2;
+  const circles = data
+    .map(d => ({ x: x(d.value), data: d }))
+    .sort((a, b) => a.x - b.x);
+  let head = null,
+    tail = null;
+
+  // Returns true if circle ⟨x,y⟩ intersects with any circle in the queue.
+  function intersects(x, y) {
+    let a = head;
+    while (a) {
+      if (radius2 - epsilon > (a.x - x) ** 2 + (a.y - y) ** 2) {
+        return true;
+      }
+      a = a.next;
+    }
+    return false;
+  }
+
+  // Place each circle sequentially.
+  for (const b of circles) {
+    // Remove circles from the queue that can’t intersect the new circle b.
+    while (head && head.x < b.x - radius2) head = head.next;
+
+    // Choose the minimum non-intersecting tangent.
+    if (intersects(b.x, (b.y = 0))) {
+      let a = head;
+      b.y = Infinity;
+      do {
+        let y1 = a.y + Math.sqrt(radius2 - (a.x - b.x) ** 2);
+        let y2 = a.y - Math.sqrt(radius2 - (a.x - b.x) ** 2);
+        if (Math.abs(y1) < Math.abs(b.y) && !intersects(b.x, y1)) b.y = y1;
+        if (Math.abs(y2) < Math.abs(b.y) && !intersects(b.x, y2)) b.y = y2;
+        a = a.next;
+      } while (a);
+    }
+
+    // Add b to the queue.
+    b.next = null;
+    if (head === null) head = tail = b;
+    else tail = tail.next = b;
+  }
+
+  return circles;
+};
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,3 @@
 export { default as parseHour } from "./parseHour";
+export { default as parseWeekHour } from "./parseWeekHour";
+export { default as dodge } from "./dodge";
