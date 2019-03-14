ba537ec458cb7233af5ba57b705a226d569eeed4
Brandon G-Home <brandon.gardner2@outlook.com>
2019-03-14 16:03:38 -0500

    OR statemetns to handle missing info

--- src/components/SearchResults/ProfileHeader.js
+++ src/components/SearchResults/ProfileHeader.js
@@ -35,38 +35,46 @@ const ProfileHeader = ({ user, isLoading, error }) => {
             className="avatar"
             width="100px"
             height="100px"
-            alt={user.name}
+            alt={user.name || user.login}
             src={user.avatar_url}
           />
         </div>
         <div className="vcard-names-container js-sticky js-user-profile-sticky-fields">
-          <h2 className="Subhead mb-1">{user.name}</h2>
+          <h2 className="Subhead mb-1">{user.name || user.login}</h2>
           <span className="text-gray f5 d-block mb-3">
-            GitHub member since {distanceInWordsToNow(new Date(user.createdAt))}{" "}
-            ago
+            GitHub member since{" "}
+            {distanceInWordsToNow(new Date(user.created_at))} ago
           </span>
           <h4 className="mt-2 Subhead">
             <Octicon icon={MarkGithub} size="small" />
-            <a className="text-blue ml-3 f5" target="_blank" href={user.url}>
+            <a
+              className="text-blue ml-3 f5"
+              target="_blank"
+              href={user.html_url}
+            >
               @{user.login}
             </a>
           </h4>
           <h4 className="mt-2 Subhead">
             <Octicon icon={Mail} size="small" />
-            <span className="ml-3 f5">{user.email}</span>
+            <span className="ml-3 f5">{user.email || "Unavailable"}</span>
           </h4>
           <h4 className="mt-2 Subhead">
             <Octicon icon={Location} size="small" />
-            <span className="ml-3 f5">{user.location}</span>
+            <span className="ml-3 f5">{user.location || "Unavailable"}</span>
           </h4>
           <h4 className="mt-2 Subhead d-block">
             <Octicon icon={Info} size="small" />
-            <span className="ml-3 f5">{user.bio}</span>
+            <span className="ml-3 f5">{user.bio || "Unavailable"}</span>
           </h4>
           <h4 className="mt-2 Subhead">
             <Octicon icon={Link} size="small" />
-            <a className="text-gray ml-3 f5" target="_blank" href={user.blog}>
-              {user.blog}
+            <a
+              className="text-gray ml-3 f5"
+              target="_blank"
+              href={user.blog || "#"}
+            >
+              {user.blog || "Unavailable"}
             </a>
           </h4>
         </div>
